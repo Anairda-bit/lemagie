f3e313e4e73da2014ddd92455cce0068a6ef1f9f
Anairda-bit <68434538+Anairda-bit@users.noreply.github.com>
2021-07-18 12:58:52 +0200

    añado creditos

---
+++ inc/creditos.php
@@ -0,0 +1,3 @@
+<div>Imagenes De <a href="https://es.vecteezy.com/vectores-gratis/tarot">Tarot Vectores por Vecteezy</a></div>
+<div>Iconos diseñados por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
+<div>Iconos diseñados por <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>

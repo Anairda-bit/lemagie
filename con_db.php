1dd25700f1e7bf3c1a7a148efb97ed946cf8d60a
csimomelian <clientesimo@gmail.com>
2021-07-18 11:56:13 +0100

    config conection db

--- con_db.php
+++ con_db.php
@@ -1,6 +1,12 @@
 <?php
-$conex = new mysqli("127.0.0.1", "root", "", "proyecto");
-if ($conex->connect_error) {
-    die('no se puede conectar con la bd');
-}
+  $host_name = 'db5003607678.hosting-data.io';
+  $database = 'dbs2938653';
+  $user_name = 'dbu233748';
+  $password = '?A@g3Ha&L$P?r2a';
+
+  $conex = new mysqli($host_name, $user_name, $password, $database);
+
+  if ($conex->connect_error) {
+    die('<p>Error al conectar con servidor MySQL: '. $conex->connect_error .'</p>');
+  }
 ?>
